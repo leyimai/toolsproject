d4e3f7edf5deffe3d43f28babe1d65034aa9060e
Leyi Mai <lm3504@columbia.edu>
2019-11-28 00:02:03 +0000

    complete the models for sightings

--- sightings/views.py
+++ sightings/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Sighting
+from django.template import loader
 
 def index(request):
-        return HttpResponse('Hi')
+    latest_squirrel_list = Sighting.objects.all()
+    template = loader.get_template('sightings/index.html')
+    context = {
+            'Latest Squirrel List': latest_squirrel_list,
+            }
+    return HttpResponse(template.render(context, request))
 
+def details(request, unique_squirrel_id):
+    sighting = Sighting.objects.get(unique_squirrel_id = unique_squirrel_id)
+    return HttpResponse(sighting.unique_squirrel_id)
+
+def add(request):
+    return HttpResponse('Hi')
+
+def stats(request):
+    return HttpResponse('Hi')
