ec26ac21a08d1c550455c803b655e23d57d7197f
Yuemeng Zhang <yz3684@columbia.edu>
2019-12-07 02:44:45 +0000

    modified add and update pages

--- sightings/views.py
+++ sightings/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Sighting
 from django.template import loader
+from django.shortcuts import redirect
+from .forms import SightingForm
 
 def index(request):
     
@@ -14,13 +16,33 @@ def index(request):
 
 
 def add(request):
-    return HttpResponse('Hi')
+    if request.method == 'POST':
+        form = SightingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/sightings/')
+    else:
+        form = SightingForm()
+    context = {
+            'form': form,
+            }
+    return render(request, 'sightings/details.html', context)
 
 def stats(request):
     return HttpResponse('Hi')
 
 def details(request, unique_squirrel_id):
     sighting = Sighting.objects.get(unique_squirrel_id = unique_squirrel_id)
-    return HttpResponse(sighting.unique_squirrel_id)
+    if request.method == 'POST':
+        form = SightingForm(request.POST, instance = sighting)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/sightings/')
+    else:
+        form = SightingForm(instance=sighting)
+    context = {
+            'form': form,
+            }
+    return render(request, 'sightings/details.html', context)
 
 
