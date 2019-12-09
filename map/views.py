15936d98fa6eb589e17da5193a2b128697b2eafb
Yuemeng Zhang <yz3684@columbia.edu>
2019-12-09 01:28:47 +0000

    map urls and views modify

--- map/views.py
+++ map/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from sightings.models import Sighting
 
 def map(request):
-    sightings = Sighting.objects.all()[:60]
+    sightings = Sighting.objects.all()[:50]
     context = {
             'sightings': sightings,
             }
