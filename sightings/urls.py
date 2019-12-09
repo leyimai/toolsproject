623ef3b71cfbef51d3e4f38646bac5098a075fb1
Yuemeng Zhang <yz3684@columbia.edu>
2019-12-09 01:20:02 +0000

    sightings urls further modification

--- sightings/urls.py
+++ sightings/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
                 path('', views.index),
-                path('add/', views.add, name='add'),
-                path('stats/', views.stats, name='stats'),
-                path('<unique_squirrel_id>/', views.details, name='update'),
+                path('add/', views.add),
+                path('stats/', views.stats),
+                path('<unique_squirrel_id>/', views.details),
                         ]
